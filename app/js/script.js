3f3f6a3bd67ee9e94c5c91120112a5cd9cc38ad1
виктория <viktoria@MacBook-Air-viktoria.local>
2022-06-13 09:37:37 +0300

    changed burger-menu

--- app/js/script.js
+++ app/js/script.js
@@ -18,3 +18,17 @@ $('.js-achievements').slick({
         }
     ]
   });
+const btn = document.querySelector('.main-menu__btn');
+const list = document.querySelector('.main-menu__list');
+const overlay = document.querySelector('.header__overlay');
+const body = document.querySelector('.body');
+
+btn.addEventListener('click', menuToggle);
+overlay.addEventListener('click', menuToggle);
+
+function menuToggle() {
+    btn.classList.toggle('show');
+    list.classList.toggle('show');
+    overlay.classList.toggle('show');
+    body.classList.toggle('no-scroll');
+}
